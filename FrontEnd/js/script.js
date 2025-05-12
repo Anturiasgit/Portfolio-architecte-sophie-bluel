fb6a5bc13ceb29c06bb5e6e6e1e1a3836ba1f79a
Anturia Hassani <anturia.hm@gmail.com>
2025-05-12 13:49:35 +0200

    fonctionnalités déconnexion terminées

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -3,12 +3,21 @@ import "./categories.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     const login = document.getElementById("login");
+    
 
     if(window.sessionStorage.getItem("token") && window.sessionStorage.getItem("userId")){
         login.textContent = "logout";
-        login.id = "logout";   
+        login.id = "logout"; 
+
+        
     }
 
+    document.getElementById("logout").addEventListener("click", (event) => {
+            event.preventDefault(); // pour empêcher un comportement par défaut éventuel
+            sessionStorage.clear(); // vide toute la mémoire sessionStorage
+            window.location.href = "index.html"; // redirige vers index.html
+        });    
+    
     
 
     
