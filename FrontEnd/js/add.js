4651bc74077a4c8c390fec58e1b98d3aabb4798c
Anturia Hassani <anturia.hm@gmail.com>
2025-05-21 23:53:31 +0200

    Options category dans add + validation formulaire

--- FrontEnd/js/add.js
+++ FrontEnd/js/add.js
@@ -3,10 +3,18 @@ import { fetchCategories } from "./categories.js";
 document.addEventListener("DOMContentLoaded", async () => {
 
     if (window.sessionStorage.getItem("token") && window.sessionStorage.getItem("userId")) {
-            const allCats = await fetchCategories(); 
+        const allCats = await fetchCategories(); 
 
         const input = document.querySelector('#file-input');
         const preview = document.querySelector("#preview");
+        const addCat = document.querySelector("#category");
+
+        allCats.forEach(category => {
+            const option = document.createElement("option");
+            option.value = category.name;
+            option.innerHTML = category.name;
+            addCat.appendChild(option);
+        });
 
         input.addEventListener('change', () => {
             const file = input.files[0];
@@ -20,12 +28,13 @@ document.addEventListener("DOMContentLoaded", async () => {
         });
 
         function checkFormValidity() {
-            const image = document.querySelector('#input-file').files[0];
+            const image = input.files[0];
             const title = document.querySelector('#title').value.trim();
             const category = document.querySelector('#category').value;
 
             const isValid = image && title && category;
             document.querySelector('#valider').disabled = !isValid;
+            console.log("isValid doit être true, isValid ===> ",isValid);
         }
 
         document.querySelectorAll('#imageInput, #title, #category').forEach(input => {
